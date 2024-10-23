f4a535711947a8d96bd0f15108d37e32695273af
DylGit3 <senddylmail27@gmail.com>
2024-10-22 19:57:11 -0700

    attack animation added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,8 +11,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var standing_shape = preload("res://Scenes/Player/stand_cshape.tres")
 var crouching_shape = preload("res://Scenes/Player/crouch_cshape.tres")
 
-var is_crouching = false;
+var is_crouching = false
 var direction_looking = 1
+
+var is_attacking = false
 
 
 func _process(_delta):
@@ -22,65 +24,71 @@ func _physics_process(delta: float):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("move_up") and is_on_floor() and is_crouching:
-		velocity.y = JUMP_VELOCITY
-		anim.play("Crouch")
-	elif Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
-		
 	
-	
-	# Handle crouch
-	if Input.is_action_just_pressed("crouch") and is_on_floor():
-		anim.play("Crouch")
-
-	if Input.is_action_just_pressed("crouch"):
-		crouch()
-	elif Input.is_action_just_released("crouch"):
-		stand_from_crouch()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction == 1:
 		direction_looking = 1
 	elif direction == -1:
 		direction_looking = -1
-
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
+		get_node("hitbox1").position.x = -32
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
-	if direction:
-		velocity.x = direction * speed
-		if velocity.y == 0:
-			if is_crouching:
-				anim.play("Crouch_Walk")
-			else:
-				anim.play("Run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		if is_crouching:
-			anim.play("Crouch")
-		else:
-			anim.play("Idle")
+		get_node("hitbox1").position.x = 0
 
-	if velocity.y > 0 and is_crouching:
-		anim.play("Crouch")
-	elif velocity.y > 0:
-		anim.play("Fall")
-	
-	if is_crouching and direction_looking == 1:
-		cshape.position.x = -2.075
-	elif is_crouching and direction_looking == -1:
-		cshape.position.x = 2.125
-	elif !is_crouching and direction_looking == 1:
-		cshape.position.x = -2.765
-	elif !is_crouching and direction_looking == -1:
-		cshape.position.x = 2.645
+	if Input.is_action_just_pressed("attackFirst"):
+		$AnimationPlayer.play("AttackFirst")
+		is_attacking = true
+		velocity.x *= .25
+		await get_tree().create_timer(0.4).timeout
+		is_attacking = false
+
+	# Handle jump.
+	if is_attacking == false:
+		if Input.is_action_just_pressed("move_up") and is_on_floor() and is_crouching:
+			velocity.y = JUMP_VELOCITY
+			anim.play("Crouch")
+		elif Input.is_action_just_pressed("move_up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			anim.play("Jump")
+			
+		# Handle crouch
+		if Input.is_action_just_pressed("crouch") and is_on_floor():
+			anim.play("Crouch")
+
+		if Input.is_action_just_pressed("crouch"):
+			crouch()
+		elif Input.is_action_just_released("crouch"):
+			stand_from_crouch()
+
+		if direction:
+			velocity.x = direction * speed
+			if velocity.y == 0:
+				if is_crouching:
+					anim.play("Crouch_Walk")
+				else:
+					anim.play("Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			if is_crouching:
+				anim.play("Crouch")
+			else:
+				anim.play("Idle")
+
+		if velocity.y > 0 and is_crouching:
+			anim.play("Crouch")
+		elif velocity.y > 0:
+			anim.play("Fall")
+		
+		if is_crouching and direction_looking == 1:
+			cshape.position.x = -2.075
+		elif is_crouching and direction_looking == -1:
+			cshape.position.x = 2.125
+		elif !is_crouching and direction_looking == 1:
+			cshape.position.x = -2.765
+		elif !is_crouching and direction_looking == -1:
+			cshape.position.x = 2.645
 	
 	move_and_slide()
 
@@ -92,7 +100,6 @@ func crouch():
 	cshape.shape = crouching_shape
 	cshape.position.x = -2.075
 	cshape.position.y = -7
-	#6.885
 
 func stand_from_crouch():
 	if !is_crouching:
