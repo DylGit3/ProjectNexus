82536327a38ded59f8a487c048227cb1acfbe012
DylGit3 <senddylmail27@gmail.com>
2024-11-19 17:18:32 -0800

    death animations

--- Scripts/level_selector.gd
+++ Scripts/level_selector.gd
@@ -10,7 +10,6 @@ func _ready() -> void:
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menu/main.tscn")
 
-
 func _on_level_one_button_pressed() -> void:
 	MenuMusic.stop()
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_one.tscn")
