3e0833607b4fa826ce361e4d573839664bd2f1b6
lalashub <raheem.talhah@gmail.com>
2024-12-10 17:27:26 -0800

    Update level_two.gd

--- Scenes/Levels/level_two.gd
+++ Scenes/Levels/level_two.gd
@@ -4,4 +4,7 @@ func _process(delta: float) -> void:
 	if get_node("Player/Player").health <= 0:
 		await get_tree().create_timer(1.5).timeout
 		$canvaslayer2/death_menu.visible = true
+<<<<<<< HEAD
 	
+=======
+>>>>>>> 00efadc2cf66d899d74fc5c9eeb168f19e260e7a
