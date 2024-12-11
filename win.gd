00efadc2cf66d899d74fc5c9eeb168f19e260e7a
DylGit3 <senddylmail27@gmail.com>
2024-12-10 17:24:17 -0800

    made you win screen

--- win.gd
+++ win.gd
@@ -10,13 +10,8 @@ func _on_body_entered(body: PhysicsBody2D):
 func _on_body_exited(body: Node2D):
 	entered = false
 
-func show_you_win_screen():
-	var you_win_screen = load("res://Scenes/Menu/you_win.tscn").instantiate()
-	get_tree().root.add_child(you_win_screen)
-
-
 func _process(delta):
 	if entered == true:
 		Game.playerHP = 5
 		if Input.is_action_just_pressed("ui_accept"):
-			show_you_win_screen()
+			get_tree().change_scene_to_file("res://Scenes/Menu/you_win.tscn")
